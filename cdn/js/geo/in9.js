2ac20663cdb7fc50c60cbd32e48044689636019a
Prototypic <aff4ads@gmail.com>
2024-09-18 11:44:14 +0300

    updated

---
+++ cdn/js/geo/in9.js
@@ -0,0 +1,19 @@
+(function () {
+	'use strict';
+
+	window.countryList = {
+		in: {
+			name: 'भारत',
+			code: 'in',
+			price: 2490,
+			oldPrice: 4980,
+			labelPrice: 'INR',
+			phoneHelper: ' उदाहरण के लिये + 91 123456789',
+			nameHelper: 'आदित्य',
+			nameError: 'नाम अपेक्षित है ',
+			phoneError: 'टेलीफ़ोन नंबर अपेक्षित है',
+			countryError: 'देश अपेक्षित है',
+			isDefault: true,
+		},
+	};
+})();
